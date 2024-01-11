23ac0d2fee9e566be03ba3bb1812e20dfc6b0d80
DanielaRomero <daniela.romero@epn.edu.ec>
2024-01-11 16:04:10 -0500

    Typescript

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+//import './topics/01-basic-types.ts'
+//import './topics/02-object-interface.ts'
+//import'./topics/03-functions'
+import './topics/04-homework-types'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+    Hola mundo
+`;
+
