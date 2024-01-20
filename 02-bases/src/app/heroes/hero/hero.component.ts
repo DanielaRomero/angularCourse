d57d8c103594d0f4848a8d507dacb3f9c4becb8b
DanielaRomero <daniela.romero@epn.edu.ec>
2024-01-20 15:52:24 -0500

    One way data binding

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -10,4 +10,23 @@ export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 45;
 
+  get getCapitalizedName(){
+    return this.name.toLocaleUpperCase();
+  }
+
+  set setCapitalizedName(name: string){
+
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(newHero: string = 'Spiderman'):void {
+    this.name = newHero;
+  }
+
+  changeAge( newAge: number = 26):void {
+    this.age = newAge;
+  }
 }
