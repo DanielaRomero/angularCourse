ccc5941a2e482f60a4b302dd69d5574b1680e2b6
DanielaRomero <daniela.romero@epn.edu.ec>
2024-01-20 21:29:03 -0500

    ngIf-else

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'SheHulk','Thor']
+  public heroNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'SheHulk','Thor'];
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
 }
